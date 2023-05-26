5b92bed65822823ad95f44da3ce8598afc24b95f
kimuraryota <fransisc2035@gmail.com>
2023-05-27 00:30:48 +0900

    railsAPI&PostgreSQL環境

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,10 +4,11 @@ set -e
 bundle install
 
 if [ ! -e "/backend/config/routes.rb" ]; then
+
   rails new . --force --api --database=postgresql --skip-git --skip-bundle
   bundle install
 fi
 
 rm -f /backend/tmp/pids/server.pid
-.
+
 exec "$@"
